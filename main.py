659b88e801b0f6a44015d010b4fc10159829cd83
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2022-06-22 20:47:00 +0800

    download book covers

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 
 from bs4 import BeautifulSoup
+import urllib
 import requests
 from pathvalidate import sanitize_filename
 
-DIR_PATH = './books'
+BOOKS_PATH = './books'
+IMAGES_PATH = './images'
 
 
 def check_for_redirect(response):
     if response.url == 'https://tululu.org/':
         raise requests.exceptions.HTTPError
 
-def get_book_info(response, id):
+def get_book_info(response):
     soup = BeautifulSoup(response.text, 'lxml')
     book_info = soup.find(id='content').find('h1').text
     title, writer = book_info.split('::')
@@ -22,26 +24,46 @@ def get_book_info(response, id):
     }
     return book_info
 
+def get_book_image_url(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_image = soup.find(class_='bookimage').find('img')['src']
+    book_image_url = urllib.parse.urljoin('https://tululu.org/', book_image)
+    return book_image_url
+
 def download_text(id, title):
     url = f'https://tululu.org/txt.php?id={id}'
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     filename = f'{id}. {sanitize_filename(title)}.txt'
-    filepath = os.path.join(DIR_PATH, filename)
+    filepath = os.path.join(BOOKS_PATH, filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+def download_image(image_url):
+    response = requests.get(image_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    filename = urllib.parse.urlsplit(image_url, allow_fragments=True).path.split('/')[-1]
+    filepath = os.path.join(IMAGES_PATH, filename)
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == "__main__":
-    os.makedirs(DIR_PATH, exist_ok=True)
+    os.makedirs(BOOKS_PATH, exist_ok=True)
+    os.makedirs(IMAGES_PATH, exist_ok=True)
     for id in range(1, 11):
         try:
             url = f'https://tululu.org/b{id}'
             response = requests.get(url)
             response.raise_for_status()
             check_for_redirect(response)
-            book_info = get_book_info(response, id)
+            book_image_url = get_book_image_url(response)
+            download_image(book_image_url)
+            book_info = get_book_info(response)
+            print(f'Заголовок: {book_info["title"]}')
+            print(book_image_url, '\n')
             download_text(id, book_info['title'])
         except requests.exceptions.HTTPError:
             continue
