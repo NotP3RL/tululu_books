0cbb6e89723474e6d63995de8cd54d811991f985
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2022-10-12 18:49:01 +0800

    Убрал запуск сервера из render_website.py

--- render_website.py
+++ render_website.py
@@ -19,6 +19,3 @@ rendered_page = template.render(
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
